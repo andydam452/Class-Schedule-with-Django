571ac4a8277707acfe80a7e151004e42fee108fc
andydam452 <damductai45@gmail.com>
2020-11-24 16:54:59 +0700

    Schedule with Django

--- Schedule/home/GA.py
+++ Schedule/home/GA.py
@@ -1,9 +1,9 @@
+import numpy as np
 import random
-from statistics import mean
-
+import math
 
 class GA:
-    def __init__(self,Generation,  ClassRoom, Subject, MeetingTime, Teacher, Date, Max_course_per_day, mutation,
+    def __init__(self, Generation,  ClassRoom, Subject, MeetingTime, Teacher, Date, Max_course_per_day, mutation,
                  numberOfPopulation):
         self.Generation = Generation
         self.ClassRoom = ClassRoom
@@ -14,7 +14,7 @@ class GA:
         self.Max_course_per_day = Max_course_per_day
         self.mutation = mutation
         self.numberOfPopulation = numberOfPopulation
-        self.population_score =[]
+        self.population_score = []
 
     def InitPopulation(self):
         generate = []
@@ -23,7 +23,8 @@ class GA:
             for i in self.Subject:
                 for j in range((self.Subject[i])):
                     a = [i, random.choice(self.ClassRoom), random.choice(self.MeetingTime),
-                         random.choice([teacher for teacher in self.Teacher if i in self.Teacher[teacher]]),
+                         random.choice(
+                             [teacher for teacher in self.Teacher if i in self.Teacher[teacher]]),
                          random.choice(self.Date)]
                     schedule.append(a)
             generate.append(schedule)
@@ -32,6 +33,12 @@ class GA:
     def calcFitness(self, individual):
         score = 0
         count_class_per_day = 0
+        #moi nguoi deu co ca day trong 1 tuan(ưu tiên)
+        name_teacher = list(set(map(lambda x: x, self.Teacher)))
+        teacher_name_schedule = list(set(map(lambda x: x[3], individual)))
+        if len(name_teacher) == len(teacher_name_schedule):
+            score -= 0.01
+
         for i in range(len(individual)):
             if (individual[i][0] not in self.Teacher[individual[i][3]]):
                 score += 3
@@ -57,8 +64,8 @@ class GA:
                     count_class_per_day += 0.5
                 else:
                     count_class_per_day = 0
-                if count_class_per_day == self.Max_course_per_day:
-                    score += 1
+                if count_class_per_day + 1 == self.Max_course_per_day:
+                    score += 0.5
 
         return score
 
@@ -70,10 +77,10 @@ class GA:
                 new_population_score.append(i)
 
         self.population_score = new_population_score
-        self.population_score = sorted(self.population_score, key=lambda x: x[-1])
+        self.population_score = sorted(
+            self.population_score, key=lambda x: x[-1])
         self.population_score = self.population_score[:self.numberOfPopulation]
         # return self.population_score[:math.floor(0.8 * len(self.population_score))]
-
 
     def laighep(self):
         for i in range((len(self.population_score) - 1)):
@@ -85,12 +92,12 @@ class GA:
             center = len(cha) // 2
             con.append(cha[0:center] + me[center:len(me)])
             con.append(self.calcFitness(con[0]))
-
+            self.population_score.append(con.copy())
 
     def dotbien(self):
         for _ in range(int(len(self.population_score) * self.mutation)):
             selection = random.choice(self.population_score).copy()
-            i = random.randint(0, 4)
+            i = random.randint(0, len(selection[0])-1)
             time = random.choice(self.MeetingTime)
             date = random.choice(self.Date)
             class_room = random.choice(self.ClassRoom)
@@ -99,6 +106,7 @@ class GA:
             new_gen[2] = time
             new_gen[4] = date
             new_child = []
+            
             new_child.append(selection[0].copy())
             new_child[0][i] = new_gen
             new_child.append(self.calcFitness(new_child[0]))
@@ -106,7 +114,7 @@ class GA:
 
     def schedule(self):
         #valid input
-        temp  = list(map(lambda x: self.Teacher[x], self.Teacher))
+        temp = list(map(lambda x: self.Teacher[x], self.Teacher))
         checksub = []
         for i in temp:
             checksub += [j for j in i]
@@ -126,15 +134,16 @@ class GA:
                 self.Teacher[i].remove(j)
 
         population = self.InitPopulation()
-        self.population_score = list(map(lambda x: [x, self.calcFitness(x)], population))
-        self.population_score = sorted(self.population_score, key=lambda x: x[-1])
+        self.population_score = list(
+            map(lambda x: [x, self.calcFitness(x)], population))
+        self.population_score = sorted(
+            self.population_score, key=lambda x: x[-1])
 
         for _ in range(self.Generation):
-            
+
             self.laighep()
-            
+
             self.dotbien()
-            
             self.chonloc()
 
             if self.population_score[0][1] <= 0:
